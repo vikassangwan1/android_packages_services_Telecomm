b541fe2797c0fe38f33d9d9e06a11887d0eef11a
vikassangwan1 <vikas724@gmail.com>
2018-06-15 05:36:00 +0530

    Flash on call Double vibration

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -31,6 +31,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.omni.OmniUtils;
 
 /**
  * Controls the ringtone player.
@@ -88,6 +89,8 @@ public class Ringer {
      * Used to track the status of {@link #mVibrator} in the case of simultaneous incoming calls.
      */
     private boolean mIsVibrating = false;
+    
+    private boolean mIsFlash = false;
 
     /** Initializes the Ringer. */
     @VisibleForTesting
@@ -185,6 +188,11 @@ public class Ringer {
         } else if (mIsVibrating) {
             Log.addEvent(foregroundCall, LogUtils.Events.SKIP_VIBRATION, "already vibrating");
         }
+        
+        if (!mIsFlash && Settings.System.getIntForUser(mContext.getContentResolver(),  Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1) {
+            OmniUtils.toggleCameraFlashOn();
+            mIsFlash = true;
+        }
 
         return shouldAcquireAudioFocus;
     }
@@ -236,6 +244,11 @@ public class Ringer {
             mIsVibrating = false;
             mVibratingCall = null;
         }
+        
+        if (mIsFlash && Settings.System.getIntForUser(mContext.getContentResolver(), Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1) {
+            OmniUtils.toggleCameraFlashOff();
+            mIsFlash = false;
+        }  
     }
 
     public void stopCallWaiting() {
